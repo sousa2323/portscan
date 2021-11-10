294fc4c6b854eb0682b3c39922c03fa09281d6e3
sousa2323 <sousanot.23@gmail.com>
2021-11-10 19:50:23 -0300

    Atualizaçao

--- portscan.py
+++ portscan.py
@@ -1,11 +1,13 @@
 import socket
 
-ports = [21,23, 80, 443, 8080]
+domain = input(str("Enter a domain..."))
+
+ports = [21, 23, 80, 443, 8080]
 
 for port in ports:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(0.1)
-    codigo = client.connect_ex(('pypress.com.br', port))
+    codigo = client.connect_ex((domain, port))
     if codigo == 0:
         print(port, "OPEN")
     else:
