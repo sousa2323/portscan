849dc548464e3f47f8633686ede975b9f81ef9e8
sousa2323 <sousanot.23@gmail.com>
2021-11-10 19:15:54 -0300

    Projeto PortScan

---
+++ portscan.py
@@ -0,0 +1,12 @@
+import socket
+
+ports = [21,23, 80, 443, 8080]
+
+for port in ports:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(0.1)
+    codigo = client.connect_ex(('pypress.com.br', port))
+    if codigo == 0:
+        print(port, "OPEN")
+    else:
+        print(port, "CLOSED")    
